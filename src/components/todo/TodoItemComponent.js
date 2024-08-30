0cb4f662c82362c522385310439e06c65699896e
Eng-Omar-Hussein <omarhussein2111@gmail.com>
2024-08-30 21:19:30 +0300

    end todo

--- src/components/todo/TodoItemComponent.js
+++ src/components/todo/TodoItemComponent.js
@@ -5,22 +5,20 @@ const TodoItemComponent = (props) => {
     const handelDelete=(e)=>{
         props.deleteTodo(parseInt(e.parentElement.parentElement.querySelector("span").innerText.split(".")[0]));
     }
-    const handelCompleted=(e)=>{
-        props.completeTodo(props.task);
-    }
     return (
-        <li className={`list-group-item ${props.task.completed ? Styles.completed : ""}`} >
-            <div>
+        <li className={`list-group-item ${props.task.completed ? Styles.completed : ""} ${Styles.background}`} >
+            <div className="overflow-hidden">
                 <span>
                     {props.task.id}. {props.task.title} - {props.task.dueDate}
                 </span>
                 <div>
-                    <button onClick={(e)=>handelCompleted(e.target)} className="btn btn-success btn-sm me-2">
+                    <button onClick={()=>props.completeTodo(props.task)} className="btn btn-success btn-sm me-2">
                         {props.task.completed ? "Undo" : "Complete"}
                     </button>
                     <button
                         disabled={props.task.completed ? true : false}
                         className="btn btn-warning btn-sm me-2"
+                        onClick={()=>props.editTodo(props.task)}
                     >
                         Edit
                     </button>
